02a95107fdae52a6191ad75d9861e36087485046
= <oaemdl@gmail.com>
2023-11-24 10:34:48 -0500

    cs

--- bean/Empleado.cs
+++ bean/Empleado.cs
@@ -1,12 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Data;
 
 namespace appPlanilla_CS_IA_LMV.bean
 {
     internal class Empleado
     {
+        #region Propiedades
+            public int id { get; set; }
+            public string Dni { get; set; }
+            public string Ruc { get; set; }
+            public string Nombres { get; set; }
+            public string ApellidoPaterno { get; set; }
+            public string ApellidoMaterno { get; set; }
+            public string FechaNacimiento { get; set; }
+            public string Direccion { get; set; }
+            public string Correo { get; set; }
+            public string Celular { get; set; }
+            public string Passwordd { get; set; }
+            public int idDomiciliado { get; set; }
+            public int idEstadoCivil { get; set; }
+            public int idNivelEducativo { get; set; }
+            public int idDepartamento { get; set; }
+            public int idProvincia { get; set; }
+            public int idDistrito { get; set; }
+        #endregion
+
+        #region Métodos
+            
+        public void setRegistro( DataRow dr )
+        {
+            if (dr == null) return;
+
+            id = int.Parse(dr["id"].ToString());
+            Dni = dr["Dni"].ToString();
+            Ruc = dr["Ruc"].ToString();
+            Nombres = dr["Nombres"].ToString();
+            ApellidoPaterno = dr["ApellidoPaterno"].ToString();
+            ApellidoMaterno = dr["ApellidoMaterno"].ToString();
+            FechaNacimiento = dr["FechaNacimiento"].ToString();
+            Direccion = dr["Direccion"].ToString();
+            Correo = dr["Dni"].ToString();
+            Celular = dr["Dni"].ToString();
+            Passwordd = dr["Dni"].ToString();
+            idDomiciliado = int.Parse(dr["idDomiciliado"].ToString());
+            idEstadoCivil = int.Parse(dr["idEstadoCivil"].ToString());
+            idNivelEducativo = int.Parse(dr["idNivelEducativo"].ToString());
+            idDepartamento = int.Parse(dr["idDepartamento"].ToString());
+            idProvincia = int.Parse(dr["idProvincia"].ToString());
+            idDistrito = int.Parse(dr["idDistrito"].ToString());
+        }
+
+        #endregion
     }
 }
